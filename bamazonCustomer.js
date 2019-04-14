710d8f171a1b9b5e1f1460dc15ff16437a672dc5
bwillstorey <bwillstorey@gmail.com>
2019-04-14 18:15:55 -0500

    functioning customer program

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,20 +15,76 @@ connection.connect(function(err) {
     buyProduct();
 });
 
-function buyProduct(selection) {
+function buyProduct() {
+
+    var query = "SELECT item_id, product_name, price FROM products";
+
+    connection.query(query, function(err, res) {
+        
+        // console.log(res);
+
+        res.forEach(function(product) {
+            console.log("ID: " + product.item_id + " | " + product.product_name + " | $" + product.price);
+        });
+    
 
     inquirer
-        .prompt({
-            
+        .prompt([
+        {
             name: "selectProduct",
-            type: "list",
-            message: "Which item would you like to buy?",
-            choices: ["Gummy Bears", "Tennis Shoes", "Windshield Wipers", "Decorative Pillows", "Hiking Boots", "Vitamins", "Airpods", "Almond Butter", "This is Spinal Tap", "iPhone Cable"]
-        })
-        .then(function() {
-            switch (selection.selectProduct) {
-                case "Gummy Bears":
+            type: "input",
+            message: "Which item would you like to buy? (enter product ID)",
+            // choices: ["Gummy Bears", "Tennis Shoes", "Windshield Wipers", "Decorative Pillows", "Hiking Boots", "Vitamins", "Airpods", "Almond Butter", "This is Spinal Tap", "iPhone Cable"]
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }
+        ])
+        .then(function(input) {
+            
+            var productSelected = input.selectProduct;
+            var quantity = input.quantity;
+            var query = "SELECT * FROM products WHERE item_id=?";
 
+            connection.query(query, input.selectProduct, function(err, res) {
+                var item = res[0];
+
+                if (item.stock_quantity >= quantity) {
+                    var newQuantity = item.stock_quantity - quantity;
+                    var updateQuantity = "UPDATE products SET stock_quantity=? WHERE item_id=?";
+                    var total = quantity * item.price;
+
+                    connection.query(updateQuantity, [newQuantity, productSelected], function(err, res) {
+                        console.log("Order completed for $" + total);
+                        keepShopping();
+                    });
+                }
+                else {
+                    console.log("Sorry - this item is out of stock.");
+                    keepShopping();
+                }
+            });
+        });
+    });
+}
+
+function keepShopping() {
+    inquirer
+        .prompt ([
+            {
+                name: "continue",
+                type: "confirm",
+                message: "Would you like to keep shopping?"
+            }
+        ])
+        .then(function(confirmed) {
+            if(confirmed.continue) {
+                buyProduct();
+            }
+            else {
+                console.log("Thank you. Come again.");
             }
         });
 }
